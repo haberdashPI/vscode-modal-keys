73d62b7159125c9e5cdb99c8d41e63476dee4a3f
David Little <david.frank.little@gmail.com>
2021-07-16 16:06:43 -0400

    fix: working build script from cli

--- docs/build.js
+++ docs/build.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const { Remarkable } = require('remarkable')
 const hljs = require('highlight.js')
 const mkdirp = require('mkdirp')
+const path = require('path')
 
 console.log(process.cwd())
 
@@ -11,19 +12,6 @@ console.log(process.cwd())
 files = glob.sync('src/*.ts').
     concat(glob.sync('docs/*.js', {ignore: "docs/build.js"})).
     concat(glob.sync('presets/*.js'))
-
-// write markdown files
-jdi.run(process.cwd(), files)
-
-// TODO: above doesn't work when script is run all together (due to
-// syncronization issues) based on
-// https://github.com/alexanderGugel/jdi/blob/master/index.js write doc files to
-// a reasonable location under docs/build (maybe even pipe directly to markdown
-// rendere without storing files)
-files.map(file => jdi.doc(path.join(process.cwd(), file))).
-    map(out => out.pipe(fs.createWriteStream()))
-    
-// fs.rmSync('docs/build.js.md', {})
 
 // markdown parser
 md = new Remarkable('full', {
@@ -41,18 +29,28 @@ md = new Remarkable('full', {
     }
 })
 
-markfiles = files.map(x => x+".md")
-
 mkdirp.sync('docs/build/src')
 mkdirp.sync('docs/build/presets')
-for(const mfile of markfiles){    
-    destfile = mfile
-    destfile = destfile.replace('docs/','docs/build/')
-    destfile = destfile.replace('src/','docs/build/src/')
-    destfile = destfile.replace('presets/','docs/build/presets/')
-    destfile = destfile.replace(/\.(ts|js).md$/, '.html')
-    
-    str = fs.readFileSync(mfile, 'utf8')
-    fs.writeFileSync(destfile, md.render(str))
+
+function docpath(source){
+    return source.replace('docs/','docs/build/')
+        .replace('src/','docs/build/src/')
+        .replace('presets/','docs/build/presets/')
+        .replace(/\.(ts|js)$/, '.html')
 }
-markfiles.forEach(f => fs.rmSync(f, {}))
+
+files.map(file => jdi.doc(path.join(process.cwd(), file))).
+    map(stream => {
+        let chunks = []
+        stream.on('data', chunk => chunks.push(Buffer.from(chunk)))
+        stream.on('error', e => {if(e){ throw e }})
+        stream.on('end', () => {
+            let out = md.render(Buffer.concat(chunks).toString('utf8'))
+            let toFile = docpath(stream.options.file)
+            fs.writeFile(toFile, out, err => {
+                if(err) throw err;
+                else console.log('Wrote '+toFile)
+            })
+        })
+    })
+    
