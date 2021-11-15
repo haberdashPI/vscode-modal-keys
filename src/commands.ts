15bee9f3ccf4b09724ce8baeaa2fb93f60241c7e
David Little <david.frank.little@gmail.com>
2021-11-14 20:15:33 -0500

    cleanup

--- src/commands.ts
+++ src/commands.ts
@@ -334,6 +334,87 @@ function keySeq(word: IKeyRecording | undefined){
     }
 }
 
+// TODO: use KeyboardLayoutMap to improve behavior
+// acorss non-english / non-standard layouts
+const keyRows = [
+    [
+        {top: "~", bottom: "`"},
+        {top: "!", bottom: "1"},
+        {top: "@", bottom: "2"},
+        {top: "#", bottom: "3"},
+        {top: "$", bottom: "4"},
+        {top: "%", bottom: "5"},
+        {top: "^", bottom: "6"},
+        {top: "&", bottom: "7"},
+        {top: "*", bottom: "8"},
+        {top: "(", bottom: "9"},
+        {top: ")", bottom: "0"},
+        {top: "_", bottom: "-"},
+        {top: "+", bottom: "="},
+        {bottom: "delete", length: '1-5'}
+    ],
+    [
+        {bottom: 'tab', length: '1-5'},
+        {top: "Q", bottom: "q"},
+        {top: "W", bottom: "w"},
+        {top: "E", bottom: "e"},
+        {top: "R", bottom: "r"},
+        {top: "T", bottom: "t"},
+        {top: "Y", bottom: "y"},
+        {top: "U", bottom: "u"},
+        {top: "I", bottom: "i"},
+        {top: "O", bottom: "o"},
+        {top: "P", bottom: "p"},
+        {top: "{", bottom: "["},
+        {top: "}", bottom: "]"},
+        {top: "|", bottom: "\\"}
+    ],
+    [    
+        {bottom: "caps lock", length: '1-75'},
+        {top: "A", bottom: "a"},
+        {top: "S", bottom: "s"},
+        {top: "D", bottom: "d"},
+        {top: "F", bottom: "f"},
+        {top: "G", bottom: "g"},
+        {top: "H", bottom: "h"},
+        {top: "J", bottom: "j"},
+        {top: "K", bottom: "k"},
+        {top: "L", bottom: "l"},
+        {top: ":", bottom: ";"},
+        {top: '"', bottom: "'"},
+        {bottom: "return", length: '1-75'}
+    ],
+    [
+        {bottom: "shift", length: '2-25'},
+        {top: "Z", bottom: "z"},
+        {top: "X", bottom: "x"},
+        {top: "C", bottom: "c"},
+        {top: "V", bottom: "v"},
+        {top: "B", bottom: "b"},
+        {top: "N", bottom: "n"},
+        {top: "M", bottom: "m"},
+        {top: "<", bottom: ","},
+        {top: ">", bottom: "."},
+        {top: "?", bottom: "/"},
+        {bottom: "shift", length: '2-25'}
+    ],
+    [
+        {}, {}, {},
+        {length: '1-25'},
+        {length: '5'},
+        {length: '1-25'},
+        {}, {}, {}, {}
+    ]
+];
+
+function get(x: any, key: string, def: any){
+    if(key in x){
+        return x[key]
+    }else{
+        return def
+    }
+}
+
 // generates the webview for a provider
 export class DocViewProvider implements vscode.WebviewViewProvider {
     public static readonly viewType = 'modalkeys.bindingView';
@@ -357,7 +438,21 @@ export class DocViewProvider implements vscode.WebviewViewProvider {
 
     public _getHtml(webview: vscode.Webview){
         let style = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'docview', 'style.css'))
-        let keyboard = "hello world!"
+        let keys = `
+            <div class="keyboard">
+                ${keyRows.map(row => `
+                    <div class="keyboard-row">
+                        ${row.map((key: any) => `
+                            <div class="key key-length-${get(key, 'length', 1)}">
+                                <div class="label">${get(key, 'top', '')}</div>
+                                <div class="label">${get(key, 'bottom', '')}</div>
+                            </div>
+                        `).join('\n')}
+                    </div>
+                `).join('\n')}
+            </div>
+        `
+
         return `
             <!DOCTYPE html>
             <html lang="en">
@@ -369,7 +464,7 @@ export class DocViewProvider implements vscode.WebviewViewProvider {
 
             </head>
             <body>
-            ${keyboard}
+            ${keys}
             </body>
             </html>
         `
